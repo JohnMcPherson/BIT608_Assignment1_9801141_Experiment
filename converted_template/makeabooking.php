33dcdaf31b949083d8fe4117ea330b96ca0b7e5b
JohnMcPherson <john.mcpherson@xtra.co.nz>
2021-12-09 14:24:17 +1300

    - remove datepicker script from makeabooking.php - create makeabookinghtml.html (allow debugging for now)

--- converted_template/makeabooking.php
+++ converted_template/makeabooking.php
@@ -16,7 +16,7 @@ $DBC = mysqli_connect("127.0.0.1", DBUSER, DBPASSWORD, DBDATABASE);
 //insert DB code from here onwards
 //check if the connection was good
 if (mysqli_connect_errno()) {
-    echo "Error: Unable to connect to MySQL. ".mysqli_connect_error() ;
+    echo "Error: Unable to connect to MySQL. " . mysqli_connect_error();
     exit; //stop processing the page further
 }
 // TODO include ajax to query the database to populate the room field
@@ -24,44 +24,80 @@ if (mysqli_connect_errno()) {
 
 //prepare a query and send it to the server
 ?>
+<body>
+<script>
+    $(document).ready(function() {
+        var dateFormat = "mm/dd/yy",
+            from = $("#checkindate")
+                .datepicker({
+                    defaultDate: "+1w",
+                    changeMonth: true,
+                    numberOfMonths: 3
+                })
+                .on("change", function () {
+                    to.datepicker("option", "minDate", getDate(this));
+                }),
+            to = $("#checkoutdate").datepicker({
+                defaultDate: "+1w",
+                changeMonth: true,
+                numberOfMonths: 3
+            })
+                .on("change", function () {
+                    from.datepicker("option", "maxDate", getDate(this));
+                });
+
+        function getDate(element) {
+            var date;
+            try {
+                date = $.datepicker.parseDate(dateFormat, element.value);
+            } catch (error) {
+                date = null;
+            }
+
+            return date;
+        }
+    });
+</script>
 <h1>Make a Booking</h1>
-<h2><a href='bookingslisting.php'>[Return to the Bookings listing]</a><a href="/bnb/converted_template/">[Return to main page]</a></h2>
+<h2><a href='bookingslisting.php'>[Return to the Bookings listing]</a><a href="/bnb/converted_template/">[Return to main
+        page]</a></h2>
 <h2>Booking for [name of logged in user]</h2>
 <form method="POST" action="makeabooking.php">
-    <div class = form_settings>
-    <p>
-        <span><label for="roomid">Room (name,type,beds):</label></span>
-        <select class ="booking" id ="roomid" name="room">
-            // the following values are test values to be
-            // removed once the back-end connection is done
-            <option value = "1">Kellie,S,5</option>
-            <option value = "2">Herman,D,5</option>
-        </select>
-    </p>
-    <p>
-        <span><label for="checkindate">Checkin date:</label></span>
-        <input class = "booking" type="date" id="checkindate" name="checkindate" required>
-    </p>
-    <p>
-        <span><label for="checkoutdate">Checkout date:</label></span>
-        <input class = "booking" type="date" id="checkoutdate" name="checkoutdate" required>
-    </p>
-    <p>
-        <span><label for="contactnumber">Contact number:</label></span>
-        <input class = "booking" type="text" id="contactnumber" name="contactnumber" required
-               pattern="^\([0-9][0-9][0-9]\) [0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]$"
-               placeholder="(###) ###-####"
-    </p>
-    <p>
-        <span><label for="bookingextras">Booking Extras:</label></span>
-        <textarea class = "booking" id="bookingextras" name="bookingextras" rows="8" cols="50"></textarea>
-    </p>
+    <div class=form_settings>
+        <p>
+            <span><label for="roomid">Room (name,type,beds):</label></span>
+            <select class="booking" id="roomid" name="room">
+                // the following values are test values to be
+                // removed once the back-end connection is done
+                <option value="1">Kellie,S,5</option>
+                <option value="2">Herman,D,5</option>
+            </select>
+        </p>
+        <p>
+            <span><label for="checkindate">Checkin date:</label></span>
+            <input class="booking" type="date" id="checkindate" name="checkindate" required>
+        </p>
+        <p>
+            <span><label for="checkoutdate">Checkout date:</label></span>
+            <input class="booking" type="date" id="checkoutdate" name="checkoutdate" required>
+        </p>
+        <p>
+            <span><label for="contactnumber">Contact number:</label></span>
+            <input class="booking" type="text" id="contactnumber" name="contactnumber" required
+                   pattern="^\([0-9][0-9][0-9]\) [0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]$"
+                   placeholder="(###) ###-####"
+        </p>
+        <p>
+            <span><label for="bookingextras">Booking Extras:</label></span>
+            <textarea class="booking" id="bookingextras" name="bookingextras" rows="8" cols="50"></textarea>
+        </p>
 
-    <p style="padding-top: 15px">
-        <span>&nbsp;</span><input class="submit" type="submit" name="booking_submitted" value="Book" />
-    </p>
+        <p style="padding-top: 15px">
+            <span>&nbsp;</span><input class="submit" type="submit" name="booking_submitted" value="Book"/>
+        </p>
     </div>
 </form>
+</body>
 <?php
 mysqli_close($DBC); //close the connection once done
 
